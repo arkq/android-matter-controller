bf39b21d27113122b3ce15aeabd886e634e89492
Marcus Better <marcus@better.se>
2025-02-15 23:30:35 -0500

    Lowercase hex encoding for OpenThread BR

--- 3p-ecosystem/src/main/java/com/google/homesampleapp/screens/thread/ThreadViewModel.kt
+++ 3p-ecosystem/src/main/java/com/google/homesampleapp/screens/thread/ThreadViewModel.kt
@@ -189,7 +189,7 @@ class ThreadViewModel @Inject constructor() : ViewModel() {
       )
     return if (response.first.responseCode in OtbrHttpClient.okResponses) {
       ThreadNetworkCredentials.fromActiveOperationalDataset(
-        BaseEncoding.base16().decode(response.second)
+        BaseEncoding.base16().lowerCase().decode(response.second)
       )
     } else {
       null
